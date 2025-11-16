01a78a583f1b534511d67c82a82c106a1af5dca7
AryanV-Coder <aryanv.india1@gmail.com>
2025-11-16 21:13:03 +0530

    added in website play feature

--- moodify/src/main/java/com/moodify/moodify/controller/MoodifyController.java
+++ moodify/src/main/java/com/moodify/moodify/controller/MoodifyController.java
@@ -5,12 +5,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOriginrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
